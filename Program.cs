46563db5c7bb43b2d54fbd961c0e808d6aa83c7c
michalstacel <michalstacel@gmail.com>
2022-10-13 19:00:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int input);
+            Console.WriteLine("Input a number: ");
+
+            for (int i = 0; i < input; i++)
+            {
+
+                for (int j = 0; j < input; j++)
+                {
+                    if (i == j || j == input - i - 1)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

0dc7b236c073c29207c331d3d7336823e99e0a30
michalstacel <michalstacel@gmail.com>
2022-10-13 19:00:11 +0100

    Changed chars an queue of asking for an intAdd project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number: ");
+            int.TryParse(Console.ReadLine(), out int input);
+      
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    if (i == j || j == input - i - 1)
+                    {
+                        Console.Write("X ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
